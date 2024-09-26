dc2b48202bd46f8171f8599b3d608d34485bfbbe
Emil Meri <em223ve@student.lnu.se>
2024-09-26 22:44:03 +0200

    Update structure

---
+++ src/CaesarCipher.js
@@ -0,0 +1,10 @@
+/**
+ * @file The CaesarCipher class.
+ * @module src/CaesarCipher
+ * @author Emil Meri <em223ve@student.lnu.se>
+ * @version 0.0.1
+ */
+
+export class CaesarCipher {
+  // class content
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+/**
+ * @file The starting point of the module.
+ * @module src/index
+ * @author Emil Meri <em223ve@student.lnu.se>
+ * @version 0.0.1
+ */
+
+import CaesarCipher from './CaesarCipher.js'
