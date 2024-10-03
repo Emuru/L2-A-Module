0bf80e515c7d507f2c6b4806e2d04ca5cdbfb28c
Emil Meri <em223ve@student.lnu.se>
2024-10-03 04:42:25 +0200

    Rename index.js

---
+++ src/ciphers.js
@@ -0,0 +1,13 @@
+/**
+ * @file The starting point of the module.
+ * @module src/index
+ * @author Emil Meri <em223ve@student.lnu.se>
+ * @version 0.0.1
+ */
+
+import { Alphabet } from './Alphabet.js'
+import { AlbertiCipher } from './AlbertiCipher.js'
+import { CaesarCipher } from './CaesarCipher.js'
+import { VigenereCipher } from './VigenereCipher.js'
+
+export { Alphabet, AlbertiCipher, CaesarCipher, VigenereCipher }
